a55d9d08377de0ad89bb80c2de8a26610ec0bae4
harshwiziq <hk23490@gmail.com>
2024-10-01 20:35:52 +0530

    delete unwanted file amend

--- test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.only('test', async ({ page }) => {
-  // await page.goto('about:blank');
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByRole('link', { name: 'Store' }).click();
-  await page.getByLabel('Browse through the new Pixel').click();
-});
